c03c2fc19059134e71b9a5fe442e7aade8a6bb66
FlorianKromer <f.kromer54@gmail.com>
2021-01-23 19:00:30 +0100

    title

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ $_SESSION['danger_message'] = null;
   <div class="bgimg-1">
     <div class="caption">
       <span class="border">Bienvenue</span><br />
-      <span class="border">A la recherche de ...</span><br />
+      <span class="border">A la recherche de qui à casser INTER ?</span><br />
       <a class="border" href="/start.php">Commencer</a>
     </div>
   </div>
